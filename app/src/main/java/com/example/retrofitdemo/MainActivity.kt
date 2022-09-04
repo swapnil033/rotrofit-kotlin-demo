cf4959789d52a91fa3cc1f4b6ff6cc2b5f015361
swapnil033 <swapnildev33@gmail.com>
2022-09-04 11:07:33 +0530

    4-sept-2022

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,10 +2,84 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.example.retrofitdemo.remote.RetrofitInstance
+import com.example.retrofitdemo.remote.RetrofitService
+import com.example.retrofitdemo.remote.model.AlbumsItem
+import com.example.retrofitdemo.databinding.ActivityMainBinding
+import com.example.retrofitdemo.remote.model.Albums
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retService: RetrofitService
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(RetrofitService::class.java)
+        //getRequestWithQueryParameters()
+        //getRequestWithPathParameters()
+        uploadAlbum()
+
     }
+
+    private fun getRequestWithQueryParameters() {
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getSortedAlbums(3)
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    val result = " " + "Album Title : ${albumsItem.title}" + "\n" +
+                            " " + "Album id : ${albumsItem.id}" + "\n" +
+                            " " + "User id : ${albumsItem.userId}" + "\n\n\n"
+                    binding.textView.append(result)
+                }
+            }
+        })
+    }
+
+
+    private fun getRequestWithPathParameters() {
+        val pathResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.getAlbum(3)
+            emit(response)
+
+        }
+
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun uploadAlbum() {
+        val album = AlbumsItem(0, "My title", 3)
+        val postResponse: LiveData<Response<AlbumsItem>> = liveData {
+            val response = retService.uploadAlbum(album)
+            emit(response)
+        }
+        postResponse.observe(this, Observer {
+            val receivedAlbumsItem = it.body()
+            val result = " " + "Album Title : ${receivedAlbumsItem?.title}" + "\n" +
+                    " " + "Album id : ${receivedAlbumsItem?.id}" + "\n" +
+                    " " + "User id : ${receivedAlbumsItem?.userId}" + "\n\n\n"
+            binding.textView.text = result
+        })
+
+    }
+
 }
--- app/src/main/java/com/example/retrofitdemo/remote/RetrofitService.kt
+++ app/src/main/java/com/example/retrofitdemo/remote/RetrofitService.kt
@@ -1,4 +1,22 @@
 package com.example.retrofitdemo.remote
 
+import com.example.retrofitdemo.remote.model.Albums
+import com.example.retrofitdemo.remote.model.AlbumsItem
+import retrofit2.Response
+import retrofit2.http.*
+
 interface RetrofitService {
+
+    @GET("/albums")
+    suspend fun getAlbums(): Response<Albums>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId: Int): Response<Albums>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") albumId: Int): Response<AlbumsItem>
+
+    @POST("/albums")
+    suspend fun uploadAlbum(@Body album: AlbumsItem): Response<AlbumsItem>
+
 }
